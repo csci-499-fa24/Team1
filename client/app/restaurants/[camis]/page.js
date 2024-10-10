05570bbaa76596fa1a0a4f86682f05d171f0e283
Hector <hectorzheng4@gmail.com>
2024-10-10 15:04:47 -0400

    Use environment variable for server URL in API fetch

--- client/app/restaurants/[camis]/page.js
+++ client/app/restaurants/[camis]/page.js
@@ -13,7 +13,7 @@ export default async function RestaurantDetails({ params }) {
   const { camis } = params;
 
   // Fetching the data from the API
-  const res = await fetch(`https://team1-server.onrender.com/api/inspections/${camis}`);
+  const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `/api/inspections/${camis}`);
   if (!res.ok) {
     return <div>Failed to load restaurant data</div>;
   }
